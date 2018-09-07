1690965de8044d54a1135d5ef04af0e2c90d7aa9
738409 <738409@ELF7IT218987.ACDM.DS.SAIT.CA>
2018-09-07 17:49:54 -0600

    laststep of week01 lab2

---
+++ src/lasthelloworld/LastHelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lasthelloworld;
+
+/**
+ *
+ * @author 738409
+ */
+public class LastHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
