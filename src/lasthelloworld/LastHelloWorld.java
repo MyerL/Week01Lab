4162bbdb598cae4b3873b478c295eaf29446e737
738409 <738409@ELF7IT218987.ACDM.DS.SAIT.CA>
2018-09-07 17:51:09 -0600

    laststep of week01 lab2

--- src/lasthelloworld/LastHelloWorld.java
+++ src/lasthelloworld/LastHelloWorld.java
@@ -16,6 +16,7 @@ public class LastHelloWorld {
      */
     public static void main(String[] args) 
     {
+        System.out.println("last");
         // TODO code application logic here
     }
     
